132b9ee3187664ec4355154a7c8b5b3492c7a661
lsandaruwanjayas <fgod406@gmail.com>
2025-02-14 15:03:08 +0530

    Adding a form to your Angular app

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,12 +4,15 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import {HousingLocation} from '../housinglocation';
-
+//import the Angular form classes
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 //Update the template property of the @Component decorator to display the value housingLocationId:
 //update the template of the DetailsComponent to display the specific data represented by the housing location for the route parameter.
+//In the DetailsComponent decorator metadata, update the imports property
+//update the template HTML
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `  <article>
   <img
     class="listing-photo"
@@ -29,6 +32,18 @@ import {HousingLocation} from '../housinglocation';
       <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
     </ul>
   </section>
+  <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName" />
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName" />
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email" />
+          <button type="submit" class="primary">Apply now</button>
+        </form>
+      </section>
 </article>`,
   styleUrl: './details.component.css'
 })
@@ -37,8 +52,22 @@ export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  //before the constructor() method, add the following code to create the form object
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
       this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+//after the constructor() method, add the following code to handle the Apply now click
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -117,4 +117,10 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+//adds a method to your app's service that receives the form data to send to the data's destination
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
+  }
 }
